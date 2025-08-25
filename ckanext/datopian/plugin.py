f6c4492366b255daaccd5b8bf5381a0f19274062
Hanny <semethanny@gmail.com>
2025-08-25 19:03:57 +0800

    fgfd

--- ckanext/datopian/plugin.py
+++ ckanext/datopian/plugin.py
@@ -1,39 +1,36 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from ckan.lib.base import BaseController, render
+from flask import Blueprint, render_template
 from ckan import model
 
 
-class InsightController(BaseController):
+insight_blueprint = Blueprint(
+    "insight", __name__
+)
 
-    def index(self):
-        # Ambil semua group yang punya tag "insight"
-        groups = (
-            model.Session.query(model.Group)
-            .filter(model.Group.state == "active")
-            .join(model.tag_table, model.Group.tags)
-            .filter(model.tag_table.c.name == "insight")
-            .all()
-        )
 
-        context = {"groups": groups}
-        return render("insight/index.html", extra_vars=context)
+@insight_blueprint.route("/insight")
+def insight_index():
+    # ambil group dengan tag "insight"
+    groups = (
+        model.Session.query(model.Group)
+        .filter(model.Group.state == "active")
+        .join(model.group_tag_table)
+        .join(model.tag_table)
+        .filter(model.tag_table.c.name == "insight")
+        .all()
+    )
+    return render_template("insight/index.html", groups=groups)
 
 
 class DatopianPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IBlueprint)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, "templates")
         toolkit.add_public_directory(config, "public")
         toolkit.add_resource("fanstatic", "datopian")
 
-    def before_map(self, map):
-        map.connect(
-            "insight",
-            "/insight",
-            controller="ckanext.datopian.plugin:InsightController",
-            action="index",
-        )
-        return map
+    def get_blueprint(self):
+        return [insight_blueprint]
