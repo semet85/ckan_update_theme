ce355592e17c4cccacb828cbea817b96266180e8
Hanny <semethanny@gmail.com>
2025-08-25 20:30:33 +0800

    fgfdgd

--- ckanext/datopian/plugin.py
+++ ckanext/datopian/plugin.py
@@ -1,22 +1,31 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from flask import Blueprint, render_template
+
+
+def hello_plugin():
+    return u'Hello from the Datopian Theme extension'
+
 
 class DatopianPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer, inherit=True)
-    plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
 
-    def update_config(self, config):
-        # Tambahkan path template & public
-        toolkit.add_template_directory(config, 'templates')
-        toolkit.add_public_directory(config, 'public')
+    # IConfigurer
 
-    def before_map(self, map):
-        controller = 'ckanext.datopian.controllers.insight:InsightController'
-        map.connect('insight_index', '/insight',
-                    controller=controller, action='index')
-        map.connect('insight_read', '/insight/{id}',
-                    controller=controller, action='read')
-        return map
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('assets',
+                             'datopian')
 
-    def after_map(self, map):
-        return map
+    # IBlueprint
+
+    def get_blueprint(self):
+        u'''Return a Flask Blueprint object to be registered by the app.'''
+        # Create Blueprint for plugin
+        blueprint = Blueprint(self.name, self.__module__)
+        blueprint.template_folder = u'templates'
+        # Add plugin url rules to Blueprint object
+        blueprint.add_url_rule('/hello_plugin', '/hello_plugin', hello_plugin)
+        return blueprint
