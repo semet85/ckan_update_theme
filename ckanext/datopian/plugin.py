8ec1a94b6dcc8a8377bf2d5f8ca123f616622494
Hanny <semethanny@gmail.com>
2025-08-25 19:56:55 +0800

    gfdg

--- ckanext/datopian/plugin.py
+++ ckanext/datopian/plugin.py
@@ -1,53 +1,22 @@
-from ckan.plugins import SingletonPlugin, implements, IConfigurer, IBlueprint
+import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from flask import Blueprint, render_template
-from ckan import model
 
-# Blueprint untuk halaman /insight
-insight_blueprint = Blueprint(
-    'datopian_insight',        # nama blueprint unik
-    __name__,
-    template_folder='templates'
-)
-
-@insight_blueprint.route("/insight", endpoint="datopian_insight_index")
-def insight_index():
-    """Tampilkan semua group yang punya extras['insight']='true'"""
-    groups = (
-        model.Session.query(model.Group)
-        .filter(model.Group.state == "active")
-        .filter(model.Group.extras.any(key="insight", value="true"))
-        .all()
-    )
-    return render_template("insight/index.html", groups=groups)
-
-# Plugin CKAN
-class DatopianPlugin(SingletonPlugin):
-    implements(IConfigurer)
-    implements(IBlueprint)
+class DatopianPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer, inherit=True)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     def update_config(self, config):
-        # Tambahkan template dan public folder
-        toolkit.add_template_directory(config, "templates")
-        toolkit.add_public_directory(config, "public")
-        toolkit.add_resource("fanstatic", "datopian")
+        # Tambahkan path template & public
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
 
-    def get_blueprint(self):
-        # Kembalikan blueprint
-        return [insight_blueprint]
+    def before_map(self, map):
+        controller = 'ckanext.datopian.controllers.insight:InsightController'
+        map.connect('insight_index', '/insight',
+                    controller=controller, action='index')
+        map.connect('insight_read', '/insight/{id}',
+                    controller=controller, action='read')
+        return map
 
-
-    def get_commands(self):
-        from . import commands
-        return [commands.insight]
-    
-    @insight_blueprint.route("/insight", endpoint="datopian_insight_index")
-    def insight_index():
-        groups = (
-            model.Session.query(model.Group)
-            .filter(model.Group.state == "active")
-            .filter(model.Group.extras.any(key="insight", value="true"))
-            .all()
-    )
-        return render_template("insight/index.html", groups=groups)
-
+    def after_map(self, map):
+        return map
